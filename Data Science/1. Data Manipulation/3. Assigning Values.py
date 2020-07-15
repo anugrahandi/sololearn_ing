1c0920bf517ee21109946fcd7131de7080ba8991
anugrahandi <anugrahandi@gmail.com>
2020-07-15 22:11:49 +0700

    update lagi

--- Data Science/1. Data Manipulation/3. Assigning Values.py
+++ Data Science/1. Data Manipulation/3. Assigning Values.py
@@ -54,7 +54,7 @@ Each column replaces with each
 ''')
 print('heights_and_ages_arr = {}'.format(heights_and_ages_arr))
 
-print('\n7.a Menggabungkan Array : Seringkali kita buat array dari 1 array baru digabungkan.')
+print('\n7.A Menggabungkan Array : Seringkali kita buat array dari 1 array baru digabungkan.')
 print('Perhatikan bahwa shape() nya (45,) tidak ada 1-baris nya.')
 print('-'*30)
 ages = [57, 61, 57, 57, 58, 57, 61, 54, 68, 51, 49, 64, 50, 48, 65, 52, 56, 46, 54, 49, 51, 47, 55, 55, 54, 42, 51, 56, 55, 51, 54, 51, 60, 62, 43, 55, 56, 61, 52, 69, 64, 46, 54, 47, 70]
@@ -68,5 +68,30 @@ print(ages_arr[:3,])
 print(ages_arr.shape)
 print(ages_arr[:3,])
 
-print('\n7.b Combining Array : reshape heights_arr to (45,1). .')
-print('Then we can stack them horizontally (by column) to get a 2darray using 'hstack':')
+print('\n7.B Combining Array : reshape heights_arr to (45,1).')
+print('Then we can stack them horizontally (by column) to get a 2darray using "hstack":')
+heights = [189, 170, 189, 163, 183, 171, 185, 168, 173, 183, 173, 173, 175, 178, 183, 193, 178, 173, 174, 183, 183, 180, 168, 180, 170, 178, 182, 180, 183, 178, 182, 188, 175, 179, 183, 193, 182, 183, 177, 185, 188, 188, 182, 185, 191]
+ages = [57, 61, 57, 57, 58, 57, 61, 54, 68, 51, 49, 64, 50, 48, 65, 52, 56, 46, 54, 49, 51, 47, 55, 55, 54, 42, 51, 56, 55, 51, 54, 51, 60, 62, 43, 55, 56, 61, 52, 69, 64, 46, 54, 47, 70]
+
+heights_arr = np.array(heights)
+ages_arr = np.array(ages)
+
+heights_arr = heights_arr.reshape((45,1))
+ages_arr = ages_arr.reshape((45,1))
+
+height_age_arr = np.hstack((heights_arr, ages_arr))
+
+a = '''
+heights_arr = np.array(heights)
+ages_arr = np.array(ages)
+
+heights_arr = heights_arr.reshape((45,1))           <-- IMPORTANT
+ages_arr = ages_arr.reshape((45,1))                 <-- IMPORTANT
+height_age_arr = np.hstack((heights_arr, ages_arr)) <-- MAIN
+
+print(height_age_arr.shape)
+print(height_age_arr[:3,])
+'''
+print(a)
+print(height_age_arr.shape)
+print(height_age_arr[:3,])
